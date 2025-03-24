37e0e5fdaf9b67d56682a1e968bcd4e1c170d52b
Habibur Rahman <habibur88454@gmail.com>
2025-03-24 11:01:21 +0600

    dynamic route added

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProductDetails({ params }: {
+    params: {
+        productId: string
+    }
+}) {
+    return (
+        <div>ProductDetails: {params.productId}</div>
+    )
+}
